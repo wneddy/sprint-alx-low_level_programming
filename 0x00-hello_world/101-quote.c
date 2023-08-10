0cf40c4aa79c5a7d2c2d3d25cfb79b28b07c8f9b
wneddy <wnedward23@gmail.com>
2023-08-10 10:51:22 -0700

    print f put

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - output in standard error
+ * Return: 1 (done)
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", stdout);
+return (1);
