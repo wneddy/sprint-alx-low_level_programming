a3a6cceaf50f02c06b68884bdc38040968e8f2a1
wneddy <wnedward23@gmail.com>
2023-08-10 11:44:17 -0700

    studio

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,12 @@
+
+#include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - output in standard error
  * Return: 1 (done)
  */
 int main(void)
 {
-fput("and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", stdout);
+write(2, "and that piece of art is useful\" - Dora Korapar, 2015-10-19\n", 59);
 return (1);
