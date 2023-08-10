f252bdb0b2a3022c78ab442304129efe8064c50a
wneddy <wnedward23@gmail.com>
2023-08-10 09:22:20 -0700

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - The instruction prints the line using printf function
+ * Return: 0 (Done)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
