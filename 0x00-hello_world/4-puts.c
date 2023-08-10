be2fc7cbcbba3956d3df5e2f88c56cfa3bef3dc3
wneddy <wnedward23@gmail.com>
2023-08-10 08:31:03 -0700

    success

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - The program outputs a given line with puts function
+ * Return: 0 (done)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
