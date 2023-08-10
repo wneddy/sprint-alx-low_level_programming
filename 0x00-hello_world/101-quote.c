21ad38ca5971fc0d07997d8f05fa50e403cd24cb
wneddy <wnedward23@gmail.com>
2023-08-10 12:08:49 -0700

    today

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
-
 #include <stdio.h>
 #include <unistd.h>
-
 /**
- * main - output in standard error
+ * main - Instruction to print standard error
  * Return: 1 (done)
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korapar, 2015-10-19\n", 59);
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
