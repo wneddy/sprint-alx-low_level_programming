60d182aa7bc2d79032ada8adfc0c614460848683
wneddy <wnedward23@gmail.com>
2023-08-11 09:40:12 -0700

    base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,11 +11,11 @@ int main(void)
 {
 	int h;
 
-	for (h = 0; h <= 9; h++)
+	for (h = 0; h < 10; h++)
 	{
 		putchar(h);
 	}
-	for (h = 'a'; h <= 'f'; h++)
+	for (h = 'a'; h < 'g'; h++)
 	{
 		putchar(h);
 	}
