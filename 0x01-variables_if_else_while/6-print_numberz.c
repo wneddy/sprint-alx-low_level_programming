7cd5779f84c0f46934cd01b6e54a31f369437466
wneddy <wnedward23@gmail.com>
2023-08-11 08:59:16 -0700

    0-9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - single digit numbers of base ten
+ *
+ * using putchar
+ *
+ * Return: 0 (termination)
+ */
+int main(void)
+{
+	int t;
+
+	for (t = 0; t < 9; t++)
+	{
+		putchar(t);
+	}
+	putchar('\n');
+	return (0);
+}
