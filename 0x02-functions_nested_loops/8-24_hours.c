32b7c4590a567d10bb1dfd7da7b7b5bc7b89d65b
wneddy <wnedward23@gmail.com>
2023-08-15 08:19:23 -0700

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * jack_bauer - jack bauer timelines
+ * use the given time set
+ *
+ */
+void jack_bauer(void)
+{
+	int y;
+	int d;
+
+	y = 0;
+
+	while (y < 24)
+	{
+		d = 0;
+		while (d < 60)
+		{
+			_putchar((y / 10) + '0');
+			_putchar((y % 10) + '0');
+			_putchar((':');
+			_putchar((d / 10) + '0');
+			_putchar((d % 10) + '0');
+			_putchar(('\n');
+			d++;
+		}
+		y++;
+	}
+}
