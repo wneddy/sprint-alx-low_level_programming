6f30486ba8f547e23870be75ce54fb1a35a32f2a
wneddy <wnedward23@gmail.com>
2023-08-15 04:18:08 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,23 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - The first task
  *
- * program to print a printing function
+ * program to print (putchar)
+ * using a printing function
  *
  * Return: 0 (terminate)
  */
 int main(void)
 {
-	putchar("_putchar,\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 
 	return (0);
 }
