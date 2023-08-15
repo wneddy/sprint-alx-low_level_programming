45de43d1e556b736b4cb7f09785de5856dcece68
wneddy <wnedward23@gmail.com>
2023-08-15 08:50:56 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - sum up the parameters
+ * @val1 : data type is int
+ *
+ * @val2 : data type is int
+ *
+ * Return: 0 (terminate)
+ */
+
+int add(int val1, int val2)
+{
+	return (val1 + val2);
+}
