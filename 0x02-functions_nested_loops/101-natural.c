d22fd1dabd549767d03db52410a9fdb9e281ee67
wneddy <wnedward23@gmail.com>
2023-08-15 10:03:18 -0700

    task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - generic multiplier
+ *
+ *
+ * Return: (terminate)
+ */
+
+
+int main(void)
+{
+	int e, sum_res = 0;
+
+	for (e = 0; e < 1024; e++)
+	{
+		if ((e % 3) == 0 || (e % 5) == 0)
+			sum_res += i;
+	}
+
+	printf("%d\n", sum_res);
+
+	return (0);
+}
