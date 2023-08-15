36dbcc7c55d607d81bc439c0416f413951ed0f89
wneddy <wnedward23@gmail.com>
2023-08-15 05:27:31 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - printing output
+ * used the prototype function
+ * output in lower function
+ *
+ */
+void print_alphabet(void)
+{
+	char alpa_b;
+
+	for (alpha_b = 'a'; alpha_b <= 'z'; alpha_b++)
+		_putchar(alpha_b);
+	_putchar('\n');
+}
