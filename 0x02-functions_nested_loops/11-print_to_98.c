141cc8a25949e8fa635f9c4890f03d27f140737d
wneddy <wnedward23@gmail.com>
2023-08-15 09:10:32 -0700

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - generating all natuaral numbers
+ * separation by commas
+ * print in order
+ * pass your number to your function
+ * @n : on start number
+ *
+ * Return: null
+ */
+
+int print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+	}
+	else if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+	}
+	printf("%d\n");
+}
