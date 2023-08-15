95df7864a6c49bca007b769ee9d7a499ebe2d5c3
wneddy <wnedward23@gmail.com>
2023-08-15 09:41:09 -0700

    task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - program to find and print
+ * sum of even-valued
+ *
+ * Return: (terminate)
+ */
+
+int main(void)
+{
+	unsigned long ser1 = 0, ser2 = 1, ser_sum;
+	float all_add;
+
+	while (1)
+	{
+		sersum = ser1 + ser2;
+		if (ser_sum > 4000000)
+			break;
+
+		if ((ser_sum % 2) == 0)
+			all_add += ser_sum;
+
+		ser1 = ser2;
+		ser1 = ser_sum;
+	}
+	printf("%.0f\n", all_add);
+
+	return (0);
+}
