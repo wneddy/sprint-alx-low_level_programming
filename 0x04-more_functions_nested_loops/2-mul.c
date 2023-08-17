d2d1319a2b068f0640cb06f4fa4f8df6bd4d8307
wneddy <wnedward23@gmail.com>
2023-08-17 05:33:57 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplication of numbers
+ * @a : argument one
+ * @b : argument two
+ *
+ * Return: result
+ */
+
+int mul(int a, int b)
+{
+	int res;
+
+	res = a * b;
+	return (res);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,4 +6,5 @@ int _putchar(char c);
 void print_number(int n);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 #endif
