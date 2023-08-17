04fb7bd6d304e806f5536bec0bd9a690a817d0c2
wneddy <wnedward23@gmail.com>
2023-08-17 07:17:51 -0700

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ * main - (fizz-buzz) test program
+ * printing one to hundred
+ *
+ * Return: 0 (terminate)
+ */
+int main(void)
+{
+	int var1
+
+	for (var1 = 1; var1 <= 100; var1++)
+	{
+		if (var1 == 1)
+		{
+			printf("%d", var1);
+		}
+		else
+		{
+			if ((var1 % 3 == 0) && (var1 % 5 == 0))
+			{
+				printf(" FizzBuzz");
+			}
+			else if (var1 % 3 == 0)
+			{
+				printf(" Fizz");
+			}
+			else if (var1 % 5 == 0)
+			{
+				printf(" Buzz");
+			}
+			else
+			{
+				printf(" %d", var1);
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
