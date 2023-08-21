be5ded4e16715557e6e3a9f75a3b027d2d339853
wneddy <wnedward23@gmail.com>
2023-08-21 05:04:08 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - pointing out to a certain variable
+ * storage location
+ * @n: set int
+ *
+ * Return: null
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
