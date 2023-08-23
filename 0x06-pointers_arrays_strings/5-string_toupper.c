4e10e319fd13b13b1ba0b80c448314163be89261
wneddy <wnedward23@gmail.com>
2023-08-23 08:16:10 -0700

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * string_toupper - convertion a
+ * string from lowercase to uppercase
+ * @arr: set array
+ * Return: res final
+ */
+
+char *string_toupper(char *arr)
+{
+	int e = 0;
+
+	while (arr[e] != '\0')
+	{
+		if (arr[e] >= 97 && arr[e] <= 122)
+		{
+			arr[e] -= 32;
+		}
+		e++;
+	}
+
+	return (res);
+}
