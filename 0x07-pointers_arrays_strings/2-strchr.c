e6c9b1c470a87e2176e20fc57ab1498a8d300e9e
wneddy <wnedward23@gmail.com>
2023-08-28 07:06:25 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - locating a certain character
+ * @s: set string
+ * @c: iterator
+ *
+ * Return: current position
+ */
+
+char *_strchr(char *s, char c)
+{
+	int l;
+
+	while (s[e] != '\0')
+	{
+		if (s[e] == c)
+		{
+			break;
+			return (s[e] == c ? (s + e) : '\0');
+		}
+	}
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,4 +4,6 @@
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 void set_string(char **s, char *to);
+char *_strchr(char *s, char c);
+
 #endif
