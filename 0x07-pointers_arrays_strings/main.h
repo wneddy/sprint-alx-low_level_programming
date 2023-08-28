72972f50f1e8c8a02c03095d0e1652f15cd724f7
wneddy <wnedward23@gmail.com>
2023-08-28 08:05:05 -0700

    task 5

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,5 +5,6 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 void set_string(char **s, char *to);
 char *_strchr(char *s, char c);
-
+unsigned int _strspn(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 #endif
