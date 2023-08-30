784ac09c31fcd3f3381a55144fd66ced28961fda
wneddy <wnedward23@gmail.com>
2023-08-30 04:21:29 -0700

    task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * factorial - basic function
+ * fo getting the ! of a number
+ * @n: the factorial number
+ *
+ * Return: factorial
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+		return factorial(n) == 1;
+}
+
