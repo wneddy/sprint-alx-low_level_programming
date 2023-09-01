96b7c2e8f5f71cf0989b2040b0cd0cb6142e3894
wneddy <wnedward23@gmail.com>
2023-09-01 04:45:03 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "stdio.h"
 
 /**
  * main - printing the program name
@@ -10,13 +10,8 @@
 
 int main(int argc, char **argv)
 {
-	if (argc > 0)
-	{
-		_putchar("Program name: %s\n", argv[0]);
-	}
-	else
-	{
-		_putchar("Program does not exist\n");
-	}
+
+	printf("Program name: %s\n", argv[0]);
+
 	return (0);
 }
