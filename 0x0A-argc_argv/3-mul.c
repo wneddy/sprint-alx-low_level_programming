adc3e6afb66745d1c0f748bdff5694cdfde640ca
wneddy <wnedward23@gmail.com>
2023-09-01 07:42:15 -0700

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,13 +13,13 @@ int main(int argc, char **argv)
 {
 	int d, c, res;
 
-	d = atoi(argv[1]);
-	c = atoi(argv[2]);
-
-	if (argc == 2)
+	if (argc <= 3)
 	{
+		d = atoi(argv[1]);
+		c = atoi(argv[2]);
 		res = d * c;
 		printf("%d\n", res);
+		return (1);
 	}
 	else
 	{
