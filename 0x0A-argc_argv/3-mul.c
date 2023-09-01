60a8947d62edad58de81b36a682ff1cda4e557ac
wneddy <wnedward23@gmail.com>
2023-09-01 07:21:26 -0700

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include "stdio.h"
+
+/**
+ * main - multplying two arguments
+ * @argc: counter
+ * @argv: storage locater
+ *
+ * Return: 0 when done
+ */
+
+int main(int argc, char **argv)
+{
+	int d, c, res;
+
+	if (argc == 2)
+	{
+		res = argv[d] * argv[c];
+		printf("%d\n", res);
+	}
+	else
+		return (-1);
+}
