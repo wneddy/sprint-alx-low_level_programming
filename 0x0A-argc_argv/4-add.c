33484479a5e9ebb89c5ae684f6106e02ffe90165
wneddy <wnedward23@gmail.com>
2023-09-01 09:09:36 -0700

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "ctype.h"
+/**
+ * main - calculating the summation
+ * of a arguments in command line
+ * @argc: counter
+ * @argv: string holder
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char **argv)
+{
+	unsigned int x;
+	int i, res;
+
+	res = 0;
+	if (argc > 1)
+	{
+		for (x = 1; x < argc; x++)
+		{
+			for (i = 0; argv[x][i]; i++)
+			{
+				if (!isdigit(argv[x][i]))
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			res += atoi(argv[x]);
+		}
+		printf("%d\n", res);
+	}
+	else
+	{
+		printf("0\n");
+	}
+	return (0);
+}
