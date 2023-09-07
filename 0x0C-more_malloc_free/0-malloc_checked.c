5c7c7fef7c8009fe015d377ceeaed7b5f6888017
wneddy <wnedward23@gmail.com>
2023-09-07 11:58:03 -0700

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - function to allocate
+ * memory using malloc
+ *
+ * @b: allocation size
+ *
+ * Return: address of space
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	char *ary;
+
+	ary = malloc(b);
+	if (b == NULL)
+	{
+		exit(98);
+		return (NULL);
+	}
+
+	return (ary);
+}
