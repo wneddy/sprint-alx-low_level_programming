4f7789cfa2f86e39d1f77c25dac3da0b3ead01df
wneddy <wnedward23@gmail.com>
2023-09-12 07:27:22 -0700

    task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * free_dog - function to free allocated
+ * space of dog
+ * @d: pointer to the struct
+ *
+ * Return: success
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
+}
