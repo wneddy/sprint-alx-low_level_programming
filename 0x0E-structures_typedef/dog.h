dc50cb4cd04f1100e32f7a4a78e964f76903a44f
wneddy <wnedward23@gmail.com>
2023-09-12 04:11:51 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - declaration of struct dog
+ * @name: dog name
+ * @age: age of the dog
+ * @owner: who owns the dog
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
