ae00d09ec509f8ff28a288d373e750b3415de827
wneddy <wnedward23@gmail.com>
2023-09-13 14:24:27 -0700

    task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function to exe a function
+ * given as para on each element
+ * @array: given array
+ * @size: array size
+ * @action: function callback
+ *
+ * Return: nill
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t s;
+
+	if (action && array)
+	{
+		s = 0;
+		while (s < size)
+		{
+			action(array[s];
+			s++;
+		}
+	}
+}
