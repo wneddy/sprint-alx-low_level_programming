953ecb64be6e36cdbf44c1f547229dbb46f625b4
wneddy <wnedward23@gmail.com>
2023-09-14 05:55:40 -0700

    task 2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,39 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_string - function to print only strings
+ * @separator: check commas
+ * @n: numbers of elements
+ *
+ * Return: strings
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	char *up;
+	unsigned int x;
+
+	va_start(ap, n);
+	x = 0;
+	while (x < n)
+	{
+		up = va_arg(ap, char *);
+		if (up)
+		{
+			printf("%s", up);
+		}
+		else
+		{
+			printf("(nil)");
+		}
+		if (x < (n - 1) && separator)
+		{
+			printf("%s", separator);
+		}
+		x++;
+	}
+	va_end(ap);
+	printf("\n");
+}
