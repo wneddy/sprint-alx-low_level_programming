0b86ad97d8617d594749ca82ed5e2c272e97c5fb
wneddy <wnedward23@gmail.com>
2023-09-14 06:21:03 -0700

    task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,47 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_all - function to act like printf
+ * @format: format specifiers
+ *
+ * Return: output
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list ap;
+	char *arr, *spt; /* arr - string, spt - separator */
+	int x;
+
+	va_start(ap, format);
+	x = 0;
+	for (x = 0; format && format[x]; x++)
+	{
+		spt = " ";
+		if (format[x + 1])
+			spt = ", ";
+		switch (format[x])
+		{
+			case 'c':
+				printf("%c%s", va_arg(ap, int), spt);
+				break;
+			case 'i':
+				printf("%d%s", va_arg(ap, int), spt);
+				break;
+			case 'f':
+				printf("%f%s", va_arg(ap, double), spt);
+				break;
+			case 's':
+				arr = va_arg(ap, char *);
+				if (!*arr || !arr)
+				{
+					arr = "(nil)";
+					printf("%s%s", arr, spt);
+					break;
+				}
+		}
+	}
+	va_end(ap);
+	printf("\n");
+}
