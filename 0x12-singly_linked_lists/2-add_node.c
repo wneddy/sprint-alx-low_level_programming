92166fe58f79fd70793da384afc4a09e853f105b
wneddy <wnedward23@gmail.com>
2023-09-21 16:30:18 -0700

    task 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * add_node - new code to
+ * the start of the (SLL)
+ * @head: pointer to point to list
+ * @str: add string
+ *
+ * Return: pointer to the element
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *n;
+	unsigned int len = 0;
+
+	while (str[len])
+	{
+		len++;
+		n = malloc(sizeof(list_t));
+	}
+
+	if (!n)
+	{
+		return (NULL);
+	}
+
+	n->str = strdup(str);
+	n->len = len;
+	n->next = (*head);
+	(*head) = n;
+	return (*head);
+}
