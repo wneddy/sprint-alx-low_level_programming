c69a50310d4fd9851075840eb3f85380e1ef4afc
wneddy <wnedward23@gmail.com>
2023-09-21 16:53:32 -0700

    task 3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+
+/**
+ * add_node_end - Addition of a new node
+ * to the end of the list
+ * @head: first address
+ * @str: inserting node
+ *
+ * Return: combination
+ **/
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *tmp_1, *tmp_2;
+	unsigned int l = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	tmp_1 = malloc(sizeof(list_t));
+	if (tmp_1 == NULL)
+		return (NULL);
+
+	tmp_1->str = strdup(str);
+	if (tmp_1->str == NULL)
+	{
+		free(tmp_1);
+		return (NULL);
+	}
+
+	while (str[l])
+	{
+		l++;
+	}
+	tmp_1->len = l;
+	tmp_1->next = NULL;
+	if (*head == NULL)
+	{
+		*head = tmp_1;
+		return (tmp_1);
+	}
+	tmp_2 = *head;
+	while (tmp_2->next)
+	{
+		tmp_2 = tmp_2->next;
+		tmp_2->next = tmp_1;
+	}
+	return (tmp_1);
+}
