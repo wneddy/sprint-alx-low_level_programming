66206a93d9c1ee9d3852ad3d86d0ca6728b837f8
wneddy <wnedward23@gmail.com>
2023-09-21 16:21:23 -0700

    changes

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,13 +13,13 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		if (!h->string)
+		if (!h->str)
 		{
 			printf("[0] (nil)\n");
 		}
 		else
 		{
-			printf("[%d] %s\n", h->length, h->string);
+			printf("[%d] %s\n", h->len, h->str);
 		}
 		h = h->next;
 		nds++;
