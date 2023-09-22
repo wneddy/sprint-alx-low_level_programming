03eebc40a845a3fb60b980673f6ad0fd93b3f8cf
wneddy <wnedward23@gmail.com>
2023-09-21 17:12:03 -0700

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+
+/**
+ * hare_tortoise - prints a string before main
+ * function.
+ */
+void hare_tortoise(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -18,12 +18,14 @@
 typedef struct list_s
 {
 	char *str;
-	unsigned int len;
-	struct list_s *next;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
 
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
-void free_list(list_t *head);
+
+
+void before_main(void) __attribute__((constructor));
 #endif
