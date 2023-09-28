4b8085198039db36d612f421af1f00160accbee2
wneddy <wnedward23@gmail.com>
2023-09-28 05:07:59 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,21 +11,23 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int res = 0;
-	int i = 0;
+	int i;
 
 	if (!b)
 	{
 		return (0);
 	}
 
-	while (b[i] != '\0')
+	for (i = 0;b[i] != '\0'; i++)
 	{
 		if (b[i] != '\0' && b[i] != '1')
-		{
 			return (0);
-		}
-		res = (res << 1) | (b[i] - '0');
-		i++;
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		res <<= 1;
+		if (b[i] == 'i')
+			res += 1;
 	}
 	return (res);
 }
