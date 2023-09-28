4f4cd7c5437bb701ab4caedd6326ca0234dee6fa
wneddy <wnedward23@gmail.com>
2023-09-28 06:20:41 -0700

    task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * clear_bit - function that clears a bit
+ * at a certain index
+ * @n: no. to clear
+ * @index: position
+ *
+ * Return: success else fail
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int move;
+	unsigned int s = sizeof(*n) * 8 - 1;
+
+	if (index > s)
+		return (-1);
+	move = ~(1 << index);
+	*n = *n &move;
+
+	return (1);
+}
